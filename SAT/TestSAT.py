b1f13faf3838b4489a6ba23e036737706861d854
noxafy <hci@gmx.de>
2021-04-15 19:25:23 +0300

    SAT: Add tests for new functions

--- SAT/TestSAT.py
+++ SAT/TestSAT.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from SAT import solve
+from SAT import solve, valid, logCon, logEq
 
 '''
 Test methods:
@@ -23,8 +23,20 @@ class TestSAT(unittest.TestCase):
     def setUp(self):
         pass
 
-    def assertSAT(self, query, true_result):
-        result, model = solve(query)
+    def assertSAT(self, f, true_result):
+        result, model = solve(f)
+        self.assertEqual(result, true_result)
+
+    def assertValid(self, f, true_result):
+        result = valid(f)
+        self.assertEqual(result, true_result)
+
+    def assertLogCon(self, f1, f2, true_result):
+        result = logCon(f1, f2)
+        self.assertEqual(result, true_result)
+
+    def assertLogEq(self, f1, f2, true_result):
+        result = logEq(f1, f2)
         self.assertEqual(result, true_result)
 
     def test_satisfiable1(self):
@@ -52,11 +64,28 @@ class TestSAT(unittest.TestCase):
         self.assertSAT('a and ~a', False)
 
     def test_unsatisfiable2(self):
-        self.assertSAT('a & ~(~a -> ~c)', False)
+        self.assertSAT('a & ~(~a → ~c)', False)
 
     def test_unsatisfiable3(self):
         self.assertSAT('(~a or b) and (a -> c) & (b -> ~c) and a', False)
 
+    def test_valid1(self):
+        self.assertValid('a → (b → a)', True)
+
+    def test_valid2(self):
+        self.assertValid('a∨b -> a∧b', False)
+
+    def test_logCon1(self):
+        self.assertLogCon('a and b', '~(b -> c) v a', True)
+
+    def test_logCon2(self):
+        self.assertLogCon('a or b', '~(b -> c) v a', False)
+
+    def test_logEq1(self):
+        self.assertLogEq('a and b', '~(b -> a) v a', False)
+
+    def test_logEq2(self):
+        self.assertLogEq('a or b', '~(b -> a) v a', True)
 
 if __name__ == '__main__':
     unittest.main()
